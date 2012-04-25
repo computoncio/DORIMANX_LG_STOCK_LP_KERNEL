cd52ce3da102a627caf0ab7143662e8e97e7e838
Seungwon Jeon <tgih.jun@samsung.com>
2012-04-25 16:18:01 +0900

    mmc: core: fix the signaling 1.8V for HS200
    Currently only 1.2V is treated for HS200 mode. If the host has only
    1.8V I/O capability not 1.2V, mmc_set_signal_voltage can't be called
    for 1.8V HS200. EXT_CSD_CARD_TYPE_SDR_1_8V needs to be considered.
    
    Signed-off-by: Seungwon Jeon <tgih.jun@samsung.com>
    Reviewed-by: Subhash Jadavani <subhashj@codeaurora.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -1055,7 +1055,7 @@ out:
  */
 static int mmc_select_hs200(struct mmc_card *card, u8 *ext_csd)
 {
-	int err = 0;
+	int err = -EINVAL;
 	struct mmc_host *host;
 
 	host = card->host;
@@ -1067,10 +1067,13 @@ static int mmc_select_hs200(struct mmc_card *card, u8 *ext_csd)
 	}
 
 	if (card->ext_csd.card_type & EXT_CSD_CARD_TYPE_SDR_1_2V &&
-	    host->caps2 & MMC_CAP2_HS200_1_2V_SDR)
-		if (mmc_set_signal_voltage(host, MMC_SIGNAL_VOLTAGE_120, 0))
-			err = mmc_set_signal_voltage(host,
-						     MMC_SIGNAL_VOLTAGE_180, 0);
+			host->caps2 & MMC_CAP2_HS200_1_2V_SDR)
+		err = mmc_set_signal_voltage(host, MMC_SIGNAL_VOLTAGE_120, 0);
+
+	if (err && card->ext_csd.card_type & EXT_CSD_CARD_TYPE_SDR_1_8V &&
+			host->caps2 & MMC_CAP2_HS200_1_8V_SDR)
+		err = mmc_set_signal_voltage(host, MMC_SIGNAL_VOLTAGE_180, 0);
+
 	/* If fails try again during next card power cycle */
 	if (err)
 		goto out;
