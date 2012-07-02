296cd59e71aff0e14906340ee31ee88700270161
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-07-02 14:26:15 +0100

    mmc: sdhci: Report failure reasons for all cases in sdhci_add_host()
    For most error conditions sdhci_add_host() will print a diagnostic
    message indicating why it failed but there are a few cases where this
    does not happen. Add error messages in these cases to aid diagnosis.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -3694,8 +3694,11 @@ int sdhci_add_host(struct sdhci_host *host)
 
 	ret = request_irq(host->irq, sdhci_irq, IRQF_SHARED,
 		mmc_hostname(mmc), host);
-	if (ret)
+	if (ret) {
+		pr_err("%s: Failed to request IRQ %d: %d\n",
+		       mmc_hostname(mmc), host->irq, ret);
 		goto untasklet;
+	}
 
 	host->irq_enabled = true;
 
@@ -3714,8 +3717,11 @@ int sdhci_add_host(struct sdhci_host *host)
 	host->led.brightness_set = sdhci_led_control;
 
 	ret = led_classdev_register(mmc_dev(mmc), &host->led);
-	if (ret)
+	if (ret) {
+		pr_err("%s: Failed to register LED device: %d\n",
+		       mmc_hostname(mmc), ret);
 		goto reset;
+	}
 #endif
 
 	mmiowb();
