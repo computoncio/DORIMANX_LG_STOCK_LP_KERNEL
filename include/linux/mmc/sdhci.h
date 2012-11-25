5a5eff3602821cb99e57ca86332cd28af1546596
Daniel Drake <dsd@laptop.org>
2012-11-25 13:01:19 -0500

    mmc: sdhci: add quirk for lack of 1.8v support
    The OLPC XO-1.75 laptop includes a SDHCI controller which is 1.8v
    capable, and it truthfully reports so in its capabilities. This
    alternate voltage is used for driving new "UHS-I" SD cards at their
    full speed.
    
    However, what the controller doesn't know is that the motherboard
    physically doesn't have a 1.8v supply available.
    
    Add a quirk so that systems such as this one can override disable
    1.8v support, adding support for UHS-I cards (by running them at
    3.3v).
    
    This avoids a problem where the system would first try to run the
    card at 1.8v, fail, and then not be able to fully reset the card
    to retry at the normal 3.3v voltage.
    
    This is more appropriate than using the MISSING_CAPS quirk, which
    is intended for cases where the SDHCI controller is actually lying
    about its capabilities, and would force us to somehow override both
    caps words from another source.
    
    Signed-off-by: Daniel Drake <dsd@laptop.org>
    Reviewed-by: Philip Rakity <prakity@nvidia.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- include/linux/mmc/sdhci.h
+++ include/linux/mmc/sdhci.h
@@ -103,6 +103,9 @@ struct sdhci_host {
 
 #define SDHCI_QUIRK2_HOST_OFF_CARD_ON			(1<<0)
 #define SDHCI_QUIRK2_HOST_NO_CMD23			(1<<1)
+/* The system physically doesn't support 1.8v, even if the host does */
+#define SDHCI_QUIRK2_NO_1_8_V				(1<<2)
+
 /*
  * Read Transfer Active/ Write Transfer Active may be not
  * de-asserted after end of transaction. Issue reset for DAT line.
