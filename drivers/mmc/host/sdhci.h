378dfe1e6a9af4421382f5360d7c735c6807245f
Dorimanx <yuri@bynet.co.il>
2015-07-01 14:15:03 +0300

    MMC: Add missing define in drivers/mmc/host/sdhci.h

--- drivers/mmc/host/sdhci.h
+++ drivers/mmc/host/sdhci.h
@@ -291,6 +291,7 @@ struct sdhci_ops {
 	void	(*hw_reset)(struct sdhci_host *host);
 	void	(*platform_suspend)(struct sdhci_host *host);
 	void	(*platform_resume)(struct sdhci_host *host);
+	void	(*platform_init)(struct sdhci_host *host);
 	void	(*check_power_status)(struct sdhci_host *host, u32 req_type);
 #define REQ_BUS_OFF	(1 << 0)
 #define REQ_BUS_ON	(1 << 1)
